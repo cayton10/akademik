0a5580037ec736a6729428ab968170eace97e3af
Obi-Wan <cayton10@live.marshall.edu>
2020-04-12 20:58:15 -0400

    LOST

--- scripts/add_Classes.php
+++ scripts/add_Classes.php
@@ -1,24 +1,31 @@
 <?
 //Find session ID for each time function is called
     session_start();
-    echo "We session value " . $_SESSION['session'];
-//Pull values from form and store in appropriate variables
-    $id = htmlspecialchars($_POST['courseAlpha']);
-    $num = htmlspecialchars($_POST['courseNum']);
-    $name = htmlspecialchars($_POST['courseName']);
-    $hours = htmlspecialchars($_POST['courseHours']);
-    $grade = htmlspecialchars($_POST['courseGrade']);
-
+    echo "We have session value of " . $_SESSION['session'] ." <br />";
+//Pull values from form and store in appropriate session variables
+    $_SESSION['id'] = htmlspecialchars($_POST['courseAlpha']);
+    $_SESSION['num'] = htmlspecialchars($_POST['courseNum']);
+    $_SESSION['name'] = htmlspecialchars($_POST['courseName']);
+    $_SESSION['hours'] = htmlspecialchars($_POST['courseHours']);
+    $_SESSION['grade'] = htmlspecialchars($_POST['courseGrade']);
     //Populate classes array from form data
-
+    $id = $_SESSION['id']; 
     //Declare classes array
     $_SESSION['classes'] = array("Course" => $id . $num, "Description" => $name, "Credit Hours" => $hours, "Grade" => $grade);
-    print_r ($_SESSION['classes']);
+    foreach($_SESSION['classes'] as $value){
+        echo "$key: $value<br />";
+    }
 /* ------------------------ ADD GRADES FROM FORM POST ----------------------- */
     //Load classes into array with all relevant information
     function addClasses($id, $num, $name, $hours, $grade=NULL){
         
-        echo $classes;
+        $id = $_SESSION['id'];
+        $num = $_SESSION['id']; 
+        $name = $_SESSION['id']; 
+        $hours = $_SESSION['id']; 
+        $grade = $_SESSION['id'];  
+        
+        echo "Class: " . $id . $num ." Description: $name Course Hours: $hours Grade: $grade";
     }
 
     ?>
