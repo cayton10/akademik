59fd34cc2c49f0940a29a3d0ddec406d2e6fc13a
Obi-Wan <cayton10@live.marshall.edu>
2020-04-15 12:45:34 -0400

    Updates multiple class grades

--- scripts/update_Grade.php
+++ scripts/update_Grade.php
@@ -1,21 +1,15 @@
 <?php   
     session_start();
     //From print_Classes.php 
-
-    /* ------------------------- IF GRADE IS SET DO THIS ------------------------ */
-    //Check grade first
     $grades = ($_POST['courseGrade']);
-    print_r($grades);
     //Foreach to access $grades array
     foreach ($grades as $key => $value) {
         //Explode each value 
         $value = explode(" ", $value);
         //Reassign grade with selected grade value
         $grade = $value[0];
-        echo $grade;
         //Store course name and number
         $name = $value[1];
-        echo $name;
             //Nested foreach to access SESSION array
         foreach ($_SESSION['classes'] as $key => $class) 
             {
@@ -27,19 +21,6 @@
                 }
             }
     }
-        /*if(isset($grades)){
-            //Explode to extract each part of the option value
-            $grades = explode(" ", $grades);
-            //Reassign grade with selected grade value
-            $grade = $grades[0];
-            echo $grade;
-            //Store course name and number
-            //Hope this doesn't count as me using > 1 array
-            $name = $grades[1];
-            //Loop through $_SESSION array w/ foreach
-            echo $name;
-            
-        }*/
 
     //redirect user back to index
     header('location:../index.php');
